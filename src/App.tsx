f2a65caa4cbe0a53beed853ceafc5c6e2969dd35
Aayush Rana <aayushranahardi@gmail.com>
2024-08-07 22:18:51 +0530

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState, ChangeEvent, FormEvent } from "react";
 
 interface FormValues {
@@ -47,6 +48,8 @@ const App: React.FC = () => {
     airportDistance: 0,
   });
 
+  const [price, setPrice] = useState(0);
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormValues((prevValues) => ({
@@ -55,23 +58,50 @@ const App: React.FC = () => {
     }));
   };
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Form values:", formValues);
+  const handleSubmit = () => {
+    axios
+      .post("http://localhost:8000/houseprice/predict", {
+        num_of_bedroom: formValues.bedrooms,
+        no_of_bathroom: formValues.bathrooms,
+        living_area: formValues.livingArea,
+        lot_area: formValues.lotArea,
+        no_of_floors: formValues.floors,
+        waterfront_prsent: formValues.waterfront,
+        no_of_views: formValues.views,
+        house_condition: formValues.condition,
+        house_grade: formValues.grade,
+        house_area_exclude_basement: formValues.houseArea,
+        area_of_basement: formValues.basementArea,
+        built_year: formValues.builtYear,
+        renovation_area: formValues.renovationYear,
+        postal_code: formValues.postalCode,
+        lattitude: formValues.latitude,
+        longitude: formValues.longitude,
+        living_area_renov: formValues.livingAreaRenov,
+        lot_area_renov: formValues.lotAreaRenov,
+        num_schools_nearby: formValues.schoolsNearby,
+        distance_area_from_airport: formValues.airportDistance,
+      })
+      .then((res) => {
+        console.log(res);
+        if (res) {
+          setPrice(res.data.predictions[0]);
+        }
+      })
+      .catch((_) => alert("There is some error. Try again"));
   };
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100 p-4">
-      <form
-        onSubmit={handleSubmit}
-        className="bg-white p-8 rounded-lg shadow-lg w-full max-w-2xl"
-      >
-        <h1 className="text-3xl font-bold mb-6 text-center">FindYourHome</h1>
+      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-2xl">
+        <h1 className="text-3xl font-bold mb-6 text-center text-black">
+          FindYourHome
+        </h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           {Object.keys(formValues).map((key) => (
             <div key={key} className="mb-4 w-full">
               <label
-                className="block text-gray-700 text-lg font-medium mb-2 text-center"
+                className="block text-black text-lg font-medium mb-2 text-center"
                 htmlFor={key}
               >
                 {key
@@ -84,29 +114,34 @@ const App: React.FC = () => {
                 id={key}
                 value={formValues[key as keyof FormValues]}
                 onChange={handleChange}
-                className="input input-bordered border-primary w-full text-lg bg-white"
+                className="input text-black input-bordered border-primary w-full text-lg bg-white"
               />
             </div>
           ))}
         </div>
         <button
           type="submit"
+          onClick={(e) => handleSubmit()}
           className="btn btn-primary w-full text-lg py-2 mt-4"
         >
           Find
         </button>
-        <div className="mt-6 w-full">
-          <label className="block text-gray-700 text-lg font-medium mb-2 text-center">
-            Price:
-          </label>
-          <input
-            type="text" // Use text to support float values
-            className="input input-bordered border-primary w-full text-lg bg-white"
-            value="0"
-            readOnly
-          />
-        </div>
-      </form>
+
+        {price !== 0 && (
+          <div className="mt-12 w-full">
+            <label className="block text-black text-lg font-medium mb-2 text-center">
+              Estimated Price is:
+             
+            </label>
+            <input
+              type="text" // Use text to support float values
+              className="input text-red-700 font-bold w-full text-lg bg-white"
+              value={'Rs. ' + price}
+              readOnly
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
